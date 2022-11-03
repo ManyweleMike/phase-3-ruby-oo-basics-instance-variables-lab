7780c8279d6ed945fc91647ad7d5071b6a2f6b0f
ManyweleMike <mugendimike80@gmail.com>
2022-11-04 01:48:13 +0300

    Complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    #Setter Method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    
+    #Getter Method
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
